baa2adc9c9bf85d319fc6cbb7cb152da42cda62a
winston <hey@winston.sh>
2023-02-14 05:16:45 +0100

    feat(cura): add desktop icon, update to 5.2.2

--- pkgs/cura/default.nix
+++ pkgs/cura/default.nix
@@ -1,13 +1,37 @@
 {
   appimageTools,
   fetchurl,
-}:
-appimageTools.wrapType1 rec {
-  name = "cura";
-  version = "5.2.1";
+  lib,
+}: let
+  pname = "cura";
+  version = "5.2.2";
+  name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/Ultimaker/Cura/releases/download/${version}/Ultimaker-Cura-${version}-linux-modern.AppImage";
-    sha256 = "sha256-0RDKjmLv1efGR6/VN0Enx7jiuA9DCKboNoaVdHZGXTg=";
+    sha256 = "sha256-GTMRrGh+2NRtdkqcoDfjZgDjIVv9kZhFpU5+/wYWEuc=";
   };
-}
+
+  appimageContents = appimageTools.extractType1 {
+    inherit name src;
+  };
+in
+  appimageTools.wrapType1 rec {
+    inherit name version src;
+
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/cura.desktop $out/share/applications/${pname}.desktop
+      install -m 444 -D ${appimageContents}/cura-icon.png $out/share/icons/hicolor/256x256/apps/${pname}.png
+
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Icon=cura-icon' 'Icon=${pname}' \
+        --replace 'Exec=UltiMaker-Cura' 'Exec=${pname}'
+    '';
+
+    meta = with lib; {
+      description = "3D printer / slicing GUI built on top of the Uranium framework ";
+      homepage = "https://github.com/Ultimaker/Cura";
+      license = licenses.lgpl3;
+      platforms = platforms.linux;
+    };
+  }
