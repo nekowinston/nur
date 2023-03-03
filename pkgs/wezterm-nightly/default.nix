16fd8cf78df90179ad418aee3911b5b8967ca07a
winston <hey@winston.sh>
2023-03-04 00:02:52 +0100

    fix(wezterm): pname & set version in postPatch

--- pkgs/wezterm-nightly/default.nix
+++ pkgs/wezterm-nightly/default.nix
@@ -4,18 +4,25 @@
   fetchFromGitHub,
 }:
 wezterm.overrideAttrs (oldAttrs: rec {
-  pname = "wezterm-nightly";
-  version = "nightly-2023-03-02";
-
-  patches = [];
+  pname = "wezterm";
+  version = "2d05f8f1f24db6370f0791e9ec3a98ad38cf8b4d";
+  name = "${pname}-${version}";
 
   src = fetchFromGitHub {
     owner = "wez";
-    repo = "wezterm";
-    rev = "2d05f8f1f24db6370f0791e9ec3a98ad38cf8b4d";
+    repo = pname;
+    rev = version;
     sha256 = "sha256-GL7vGne8fQIcu2u2lc5WOcPJfF6jlUZl3kKUPfkJEjc=";
     fetchSubmodules = true;
   };
+
+  patches = [];
+
+  postPatch = ''
+    echo ${version} > .tag
+    # tests are failing with: Unable to exchange encryption keys
+    rm -r wezterm-ssh/tests
+  '';
 
   cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
     name = "wezterm-nightly-vendor.tar.gz";
