26ba3b6c399aecda03f6c3108be6e7a24a20971a
winston <hey@winston.sh>
2023-03-14 05:20:11 +0100

    feat: automate wezterm nightly

---
+++ meta/wezterm-nightly.nix
@@ -0,0 +1,7 @@
+{
+  owner = "wez";
+  repo = "wezterm";
+  rev = "4afedd626dadd15d9c2929bab0e2063b54f61393";
+  sha256 = "sha256-C0tczggsqEYLcRtc5azNrwECujgBSa2ASw8talamCvM=";
+  cargoSha256 = "sha256-XJAeMDwtLtBzHMU/cb3lZgmcw5F3ifjKzKVmuP85/RY=";
+}
--- pkgs/wezterm-nightly/default.nix
+++ pkgs/wezterm-nightly/default.nix
@@ -2,31 +2,33 @@
   lib,
   wezterm,
   fetchFromGitHub,
-}:
-wezterm.overrideAttrs (oldAttrs: rec {
-  pname = "wezterm";
-  version = "fe6280f082a845c404c648ee28573f6f5680cb49";
-  name = "${pname}-${version}";
+}: let
+  inherit (import ../../meta/wezterm-nightly.nix) rev sha256 cargoSha256;
+in
+  wezterm.overrideAttrs (oldAttrs: rec {
+    pname = "wezterm";
+    version = rev;
+    name = "${pname}-${version}";
 
-  src = fetchFromGitHub {
-    owner = "wez";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-/earlE0WN2+OSc01PCGIDPE6attJBW2q+HWBuRJMkZo=";
-    fetchSubmodules = true;
-  };
+    src = fetchFromGitHub {
+      owner = "wez";
+      repo = pname;
+      rev = version;
+      inherit sha256;
+      fetchSubmodules = true;
+    };
 
-  patches = [];
+    patches = [];
 
-  postPatch = ''
-    echo ${version} > .tag
-    # tests are failing with: Unable to exchange encryption keys
-    rm -r wezterm-ssh/tests
-  '';
+    postPatch = ''
+      echo ${version} > .tag
+      # tests are failing with: Unable to exchange encryption keys
+      rm -r wezterm-ssh/tests
+    '';
 
-  cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
-    name = "wezterm-nightly-vendor.tar.gz";
-    inherit src;
-    outputHash = "sha256-7V9NRZ3QU8aCy0ZQhpv+OdmP/7Ly47zNxn02kp8kvcA=";
-  });
-})
+    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
+      name = "wezterm-nightly-vendor.tar.gz";
+      inherit src;
+      outputHash = cargoSha256;
+    });
+  })
