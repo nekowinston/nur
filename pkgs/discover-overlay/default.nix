9a8861b5367f4242973fdae7aa2a6f51e34fa90d
winston <hey@winston.sh>
2023-05-13 05:12:55 +0200

    feat: Discover Overlay for Wayland, update

--- pkgs/discover-overlay/default.nix
+++ pkgs/discover-overlay/default.nix
@@ -1,21 +1,39 @@
 {
+  fetchFromGitHub,
   gobject-introspection,
+  gtk-layer-shell,
   gtk3,
   lib,
   python3Packages,
   wrapGAppsHook,
 }:
-python3Packages.buildPythonApplication rec {
+python3Packages.buildPythonApplication {
   pname = "discover-overlay";
-  version = "0.6.3";
+  version = "2023.05.13";
 
-  src = python3Packages.fetchPypi {
-    inherit version pname;
-    sha256 = "sha256-77oCS/MEPiHSVmoSNk1rFu0XLQLqLRSqJlj7ijQMC4A=";
+  src = fetchFromGitHub {
+    repo = "Discover";
+    owner = "trigg";
+    rev = "f7058f717152f13a56303cc472d7d7e646bdf915";
+    sha256 = "sha256-YXxOh28YSBXPUt1OR9bhuglxQHPx5JsHDbX1CqIyQqg=";
   };
 
-  nativeBuildInputs = [wrapGAppsHook gobject-introspection];
-  propagatedBuildInputs = with python3Packages; [gobject-introspection gtk3 pillow pygobject3 pyxdg requests setuptools websocket-client xlib];
+  nativeBuildInputs = [
+    wrapGAppsHook
+    gobject-introspection
+  ];
+  propagatedBuildInputs = with python3Packages; [
+    gobject-introspection
+    gtk-layer-shell
+    gtk3
+    pillow
+    pygobject3
+    pyxdg
+    requests
+    setuptools
+    websocket-client
+    xlib
+  ];
 
   doCheck = false;
 
