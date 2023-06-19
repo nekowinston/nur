c4807f1750f48f2b99233eb095c7a45a4f3d7626
winston <hey@winston.sh>
2023-06-19 22:49:35 +0200

    feat(mac-wallpaper): init

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./darkman.nix
+    ./mac-wallpaper.nix
   ];
 }
---
+++ modules/hm/mac-wallpaper.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.home.mac-wallpaper;
+in {
+  options.home.mac-wallpaper = mkOption {
+    type = types.nullOr types.path;
+    default = null;
+    example = "/Users/winston/Pictures/Wallpapers/wallpaper.png";
+    description = "Path to the wallpaper to set.";
+  };
+
+  config =
+    mkIf (cfg != null && pkgs.stdenv.isDarwin)
+    {
+      home.activation.set-wallpaper = let
+        killall = lib.getExe pkgs.killall;
+        sqlite = lib.getExe pkgs.sqlite;
+      in
+        lib.hm.dag.entryAfter ["writeBoundary"] ''
+          dpdb="$HOME/Library/Application Support/Dock/desktoppicture.db"
+
+          ${sqlite} "$dpdb" "insert into data values ('${cfg}');"
+          new_entry=$(${sqlite} "$dpdb" "select max(rowid) from data;")
+          pics=$(${sqlite} "$dpdb" "select rowid from pictures")
+
+          ${sqlite} "$dpdb" "delete from preferences;"
+
+          for pic in $pics; do
+            ${sqlite} "$dpdb" "insert into preferences (key, data_id, picture_id) values(1, $new_entry, $pic)"
+          done
+
+          ${killall} Dock
+        '';
+    };
+}
