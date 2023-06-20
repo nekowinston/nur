c9275bb6153bd27d5db2d38a5910646498a6208d
winston <hey@winston.sh>
2023-06-20 20:52:35 +0200

    feat(overlays): add catppuccin-gtk

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,13 @@
 final: prev: {
+  catppuccin-gtk = prev.catppuccin-gtk.overrideAttrs (final: rec {
+    version = "0.6.0";
+    src = prev.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "gtk";
+      rev = "v${version}";
+      sha256 = "sha256-3HplAmlj8hK9Myy8mgvR88sMa2COmYAU75Fk1JuKtMc=";
+    };
+  });
   yabai = prev.yabai.overrideAttrs (oldAttrs: let
     version = "5.0.6";
     sources = {
