a40d451986379006e3a1d6f96964cae41fb5db0b
winston <hey@winston.sh>
2023-07-08 21:09:43 +0200

    fix(flake): make overlays/modules non-system specific

--- flake.nix
+++ flake.nix
@@ -28,36 +28,40 @@
   };
 
   outputs = {nixpkgs, ...} @ inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system: let
-      inherit (inputs) wezterm-src;
-      craneLib = inputs.crane.lib.${system};
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      packages = {
-        apple-cursor = pkgs.callPackage ./pkgs/apple-cursor {};
-        cura = pkgs.callPackage ./pkgs/cura {};
-        discover-overlay = pkgs.callPackage ./pkgs/discover-overlay {};
-        gonvim-tools = pkgs.callPackage ./pkgs/gonvim-tools {};
-        helm-ls = pkgs.callPackage ./pkgs/helm-ls {};
-        icat = inputs.icat.packages.${system}.icat;
-        jq-lsp = pkgs.callPackage ./pkgs/jq-lsp {};
-        mopidy-podcast-itunes = pkgs.callPackage ./pkgs/mopidy-podcast-itunes {};
-        picom-ft-labs = pkgs.callPackage ./pkgs/picom-ft-labs {};
-        plymouth-theme-catppuccin = pkgs.callPackage ./pkgs/plymouth-theme-catppuccin {};
-        posy-improved-cursor = pkgs.callPackage ./pkgs/posy-improved-cursor {};
-        swww = pkgs.callPackage ./pkgs/swww {};
-        wezterm-nightly = pkgs.darwin.apple_sdk_11_0.callPackage ./pkgs/wezterm-nightly {
-          stdenv = with pkgs;
-            if stdenv.isDarwin
-            then darwin.apple_sdk_11_0.stdenv
-            else stdenv;
-          inherit wezterm-src craneLib;
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        inherit (inputs) wezterm-src;
+        craneLib = inputs.crane.lib.${system};
+        pkgs = import nixpkgs {
+          inherit system;
         };
-      };
+      in {
+        packages = {
+          apple-cursor = pkgs.callPackage ./pkgs/apple-cursor {};
+          cura = pkgs.callPackage ./pkgs/cura {};
+          discover-overlay = pkgs.callPackage ./pkgs/discover-overlay {};
+          gonvim-tools = pkgs.callPackage ./pkgs/gonvim-tools {};
+          helm-ls = pkgs.callPackage ./pkgs/helm-ls {};
+          icat = inputs.icat.packages.${system}.icat;
+          jq-lsp = pkgs.callPackage ./pkgs/jq-lsp {};
+          mopidy-podcast-itunes = pkgs.callPackage ./pkgs/mopidy-podcast-itunes {};
+          picom-ft-labs = pkgs.callPackage ./pkgs/picom-ft-labs {};
+          plymouth-theme-catppuccin = pkgs.callPackage ./pkgs/plymouth-theme-catppuccin {};
+          posy-improved-cursor = pkgs.callPackage ./pkgs/posy-improved-cursor {};
+          swww = pkgs.callPackage ./pkgs/swww {};
+          wezterm-nightly = pkgs.darwin.apple_sdk_11_0.callPackage ./pkgs/wezterm-nightly {
+            stdenv = with pkgs;
+              if stdenv.isDarwin
+              then darwin.apple_sdk_11_0.stdenv
+              else stdenv;
+            inherit wezterm-src craneLib;
+          };
+        };
+      }
+    )
+    // {
       darwinModules.default = import ./modules/darwin;
       homeManagerModules.default = import ./modules/hm;
       overlays.default = import ./overlays;
-    });
+    };
 }
