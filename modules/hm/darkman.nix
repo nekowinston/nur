4840af2756e897d03eb213096763ce0c589cce94
winston <hey@winston.sh>
2023-07-12 05:09:06 +0200

    feat(darkman): script options, assertions

--- modules/hm/darkman.nix
+++ modules/hm/darkman.nix
@@ -3,81 +3,150 @@
   lib,
   pkgs,
   ...
-}: let
-  inherit (lib) types;
-
+}:
+with lib; let
   cfg = config.services.darkman;
+
   configModule = types.submodule {
     options = {
-      lat = lib.mkOption {
-        type = types.nullOr types.float;
+      lat = mkOption {
+        type = with types; nullOr float;
         default = null;
-        description = "Latitude to use for geoclue.";
+        description = ''
+          Your current latitude, between <literal>-90.0</literal> and
+          <literal>90.0</literal>. Must be provided along with
+          longitude.
+        '';
       };
-      lng = lib.mkOption {
-        type = types.nullOr types.float;
+      lng = mkOption {
+        type = with types; nullOr float;
         default = null;
-        description = "Longitude to use for geoclue.";
+        description = ''
+          Your current longitude, between <literal>-180.0</literal> and
+          <literal>180.0</literal>. Must be provided along with
+          latitude.
+        '';
       };
-      useGeoclue = lib.mkOption {
-        type = types.bool;
-        default = true;
-        description = "Whether to use a local geoclue instance to determine the current location. On some distributions/setups, this may require setting up a geoclue agent to function properly. The default for this will change to false in v2.0.";
+      useGeoclue = mkOption {
+        type = with types; nullOr bool;
+        default = false;
+        description = ''
+          Whether to use a local geoclue instance to determine the current location.
+
+          You must also enable the system-wide geoclue2 service on NixOS:
+          <literal>services.geoclue.enable = true;</literal>
+        '';
       };
-      useDbus = lib.mkOption {
-        type = types.bool;
+      useDbus = mkOption {
+        type = with types; nullOr bool;
         default = true;
-        description = "Whether to expose the current mode via darkman's own D-Bus API. The command line tool uses this API to apply changes, so it will not work if this setting is disabled. ";
+        description = ''
+          Whether to expose the current mode via darkman's own D-Bus API.
+          The command line tool uses this API to apply changes, so it will not work if this setting is disabled.
+
+          You will need to add <literal>pkgs.darkman</literal> to your
+          NixOS <literal>services.dbus.packages</literal> for this to work.
+        '';
       };
-      portal = lib.mkOption {
-        type = types.bool;
+      portal = mkOption {
+        type = with types; nullOr bool;
         default = true;
-        description = "Whether to use the portal for communication.";
+        description = ''
+          Whether to use the portal for communication.
+
+          You will need to add <literal>pkgs.darkman</literal> to your
+          NixOS <literal>xdg.portal.extraPortals</literal> for this to work.
+        '';
       };
     };
   };
+
+  buildActivationDrv = {
+    dark,
+    light,
+  }: let
+    shebang = ''
+      #!/usr/bin/env bash
+      set -euo pipefail
+    '';
+  in
+    pkgs.runCommand "darkman-activation" {} ''
+      mkdir -p $out/dark-mode.d $out/light-mode.d
+      echo "${shebang + dark}" > $out/dark-mode.d/nix-hm-module-darkman.sh
+      echo "${shebang + light}" > $out/light-mode.d/nix-hm-module-darkman.sh
+      chmod +x $out/dark-mode.d/nix-hm-module-darkman.sh $out/light-mode.d/nix-hm-module-darkman.sh
+    '';
 in {
   options.services.darkman = {
-    enable = lib.mkEnableOption "darkman";
-    package = lib.mkPackageOption pkgs "darkman" {};
-    config = lib.mkOption {
+    enable = mkEnableOption "darkman";
+    package = mkPackageOption pkgs "darkman" {};
+    config = mkOption {
       type = configModule;
       default = {};
       description = "Config for darkman.";
     };
-  };
-
-  config = lib.mkIf cfg.enable {
-    home.packages = [cfg.package];
-    systemd.user.services.darkman = {
-      Unit = {
-        Description = "Framework for dark-mode and light-mode transitions.";
-        Documentation = ["man:darkman(1)"];
+    activationScript = {
+      dark = mkOption {
+        type = types.str;
+        default = "";
+        description = "Commands to run when dark mode is activated. The interpreter is bash.";
       };
-      Service = {
-        Type = "dbus";
-        BusName = "nl.whynothugo.darkman";
-        ExecStart = "${lib.getExe cfg.package} run";
-        Restart = "on-failure";
-        TimeoutStopSec = 15;
-        Slice = "background.slice";
-        # Security hardening
-        LockPersonality = "yes";
-        RestrictNamespaces = "yes";
-        SystemCallArchitectures = "native";
-        SystemCallFilter = "@system-service @timer mincore";
-        MemoryDenyWriteExecute = "yes";
+      light = mkOption {
+        type = types.str;
+        default = "";
+        description = "Commands to run when light mode is activated. The interpreter is bash.";
       };
-      Install.WantedBy = ["default.target"];
     };
-
-    xdg.configFile."darkman/config.yaml".text = builtins.toJSON ({
-        inherit (cfg.config) portal;
-        dbusserver = cfg.config.useDbus;
-        usegeoclue = cfg.config.useGeoclue;
-      }
-      // lib.optionalAttrs (cfg.config.lat != null && cfg.config.lng != null) {
-        inherit (cfg.config) lat lng;
-      });
   };
+
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      assertions = [
+        (hm.assertions.assertPlatform "services.darkman" pkgs platforms.linux)
+        {
+          assertion = cfg.config.useGeoclue || (cfg.config.lat != null || cfg.config.lng != null);
+          message = "You're not using geoclue, and have not set a latitude and longitude.
+        Please either enable geoclue, or set lat and lng to the coordinates of your location.";
+        }
+      ];
+
+      home.packages = [cfg.package];
+      systemd.user.services.darkman = {
+        Unit = {
+          Description = "Framework for dark-mode and light-mode transitions.";
+          Documentation = ["man:darkman(1)"];
+        };
+        Service = {
+          Type = "dbus";
+          BusName = "nl.whynothugo.darkman";
+          ExecStart = "${lib.getExe cfg.package} run";
+          Restart = "on-failure";
+          TimeoutStopSec = 15;
+          Slice = "background.slice";
+          # Security hardening
+          LockPersonality = "yes";
+          RestrictNamespaces = "yes";
+          SystemCallArchitectures = "native";
+          SystemCallFilter = "@system-service @timer mincore";
+          MemoryDenyWriteExecute = "yes";
+          Environment = "PATH=${lib.makeBinPath (with pkgs; [bash coreutils gnugrep gnused which])}";
+        };
+        Install.WantedBy = ["default.target"];
+      };
+
+      xdg.configFile."darkman/config.yaml".text = builtins.toJSON ({
+          inherit (cfg.config) portal;
+          dbusserver = cfg.config.useDbus;
+          usegeoclue = cfg.config.useGeoclue;
+        }
+        // lib.optionalAttrs (cfg.config.lat != null && cfg.config.lng != null) {
+          inherit (cfg.config) lat lng;
+        });
+    }
+    (lib.mkIf (cfg.activationScript.dark != "" || cfg.activationScript.light != "") {
+      xdg.systemDirs.data = [
+        "${(buildActivationDrv {inherit (cfg.activationScript) dark light;})}"
+      ];
+    })
+  ]);
 }
