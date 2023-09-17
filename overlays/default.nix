f2675c56a9cfdefa1e01b2aa46e149059290b1e7
winston <hey@winston.sh>
2023-09-17 04:22:02 +0200

    feat!: drop yabai overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,17 +4,4 @@ final: prev: {
   in {
     inherit (nvfetcher) src version;
   });
-  yabai = prev.yabai.overrideAttrs (oldAttrs: let
-    nvfetcher = prev.callPackage ../../_sources/generated.nix {};
-    # the x86 build here uses the git fetch, aarch64 uses the release
-    sources = {
-      "aarch64-darwin" = nvfetcher.yabai-aarch64;
-      "x86_64-darwin" = nvfetcher.yabai-x86_64;
-    };
-    # take the version from the git tags
-    version = nvfetcher.yabai-x86_64.version;
-  in {
-    inherit version;
-    src = sources."${prev.stdenv.hostPlatform.system}" or (throw "Unsupported platform");
-  });
 }
