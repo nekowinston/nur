b0120d0effa5f769f1b202e92fc0c49b93689e13
winston <hey@winston.sh>
2024-08-27 00:13:46 +0200

    feat!: reenable sizzy

--- pkgs/sizzy/default.nix
+++ pkgs/sizzy/default.nix
@@ -1,13 +1,13 @@
 {
   appimageTools,
   callPackage,
-  glib,
   lib,
   makeWrapper,
   stdenvNoCC,
   undmg,
-}: let
-  nvfetcher = callPackage ../../_sources/generated.nix {};
+}:
+let
+  nvfetcher = callPackage ../../_sources/generated.nix { };
   pname = "sizzy";
 
   appimageContents = appimageTools.extractType2 {
@@ -19,18 +19,26 @@
     description = "The browser for web developers";
     homepage = "https://sizzy.co";
     license = licenses.unfree;
-    platforms = platforms.unix;
+    platforms = [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
   };
 
-  darwinBuilder = arch: let
-    source = nvfetcher.${"sizzy-darwin-" + arch};
-  in
+  darwinBuilder =
+    arch:
+    let
+      source = nvfetcher.${"sizzy-darwin-" + arch};
+    in
     stdenvNoCC.mkDerivation {
       inherit pname meta;
       inherit (source) version;
 
-      src = source.src.overrideAttrs (_: {name = "Sizzy.dmg";});
-      buildInputs = [undmg];
+      src = source.src.overrideAttrs (_: {
+        name = "Sizzy.dmg";
+      });
+      buildInputs = [ undmg ];
       sourceRoot = ".";
 
       installPhase = ''
@@ -39,32 +47,31 @@
       '';
     };
 in
-  {
-    aarch64-linux = throw "Unsupported platform ${stdenvNoCC.hostPlatform.system}";
-    x86_64-linux = appimageTools.wrapType2 rec {
-      inherit pname meta;
-      inherit (nvfetcher.sizzy-linux) version src;
-      name = "${pname}-${version}";
+if stdenvNoCC.isDarwin then
+  darwinBuilder (
+    if stdenvNoCC.isAarch64 then
+      "arm"
+    else if stdenvNoCC.isx86_64 then
+      "x64"
+    else
+      (throw "${pname}: ${stdenvNoCC.system} is unsupported.")
+  )
+else
+  appimageTools.wrapType2 {
+    inherit pname meta;
+    inherit (nvfetcher.sizzy-linux) version src;
 
-      extraInstallCommands = ''
-        mv $out/bin/{${name},${pname}}
+    extraInstallCommands = ''
+      source "${makeWrapper}/nix-support/setup-hook"
+      wrapProgram $out/bin/${pname} \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
 
-        source "${makeWrapper}/nix-support/setup-hook"
-        wrapProgram $out/bin/${pname} \
-          --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+      install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+      for size in 16 32 48 64 128 256 512; do
+        install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/''${size}x''${size}/apps/${pname}.png $out/share/icons/hicolor/''${size}x''${size}/apps/${pname}.png
+      done
 
-        install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
-        for size in 16 32 48 64 128 256 512; do
-          install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/''${size}x''${size}/apps/${pname}.png $out/share/icons/hicolor/''${size}x''${size}/apps/${pname}.png
-        done
-
-        substituteInPlace $out/share/applications/${pname}.desktop \
-          --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname}'
-      '';
-    };
-
-    aarch64-darwin = darwinBuilder "arm";
-    x86_64-darwin = darwinBuilder "x64";
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname}'
+    '';
   }
-  .${stdenvNoCC.hostPlatform.system}
-  or (throw "Unsupported platform ${stdenvNoCC.hostPlatform.system}")
