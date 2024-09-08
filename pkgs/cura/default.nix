585a4099db498d99482b8eabd580eb8227f42f51
winston <hey@winston.sh>
2024-09-08 02:03:22 +0200

    fix: use wrapType2 for cura

--- pkgs/cura/default.nix
+++ pkgs/cura/default.nix
@@ -2,31 +2,35 @@
   appimageTools,
   callPackage,
   lib,
-}: let
-  nvfetcher = (callPackage ../../_sources/generated.nix {}).cura;
+}:
+let
+  nvfetcher = (callPackage ../../_sources/generated.nix { }).cura;
   inherit (nvfetcher) pname version src;
   name = "${pname}-${version}";
 
-  appimageContents = appimageTools.extractType1 {
-    inherit name src;
-  };
+  appimageContents = appimageTools.extractType1 { inherit name src; };
 in
-  appimageTools.wrapType1 {
-    inherit name version src;
+appimageTools.wrapType2 {
+  inherit name version src;
 
-    extraInstallCommands = ''
-      install -m 444 -D ${appimageContents}/cura.desktop $out/share/applications/${pname}.desktop
-      install -m 444 -D ${appimageContents}/cura-icon.png $out/share/icons/hicolor/256x256/apps/${pname}.png
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/com.ultimaker.cura.desktop $out/share/applications/${pname}.desktop
+    install -m 444 -D ${appimageContents}/cura-icon.png $out/share/icons/hicolor/256x256/apps/${pname}.png
 
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace 'Icon=cura-icon' 'Icon=${pname}' \
-        --replace 'Exec=UltiMaker-Cura' 'Exec=${pname}'
-    '';
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace 'Icon=cura-icon' 'Icon=${pname}' \
+      --replace 'Exec=UltiMaker-Cura' 'Exec=${pname}-${version}'
+  '';
 
-    meta = with lib; {
-      description = "3D printer / slicing GUI built on top of the Uranium framework ";
-      homepage = "https://github.com/Ultimaker/Cura";
-      license = licenses.lgpl3;
-      platforms = platforms.linux;
-    };
-  }
+  passthru = {
+    inherit appimageContents;
+  };
+
+  meta = {
+    mainProgram = "${pname}-${version}";
+    description = "3D printer / slicing GUI built on top of the Uranium framework ";
+    homepage = "https://github.com/Ultimaker/Cura";
+    license = lib.licenses.lgpl3;
+    platforms = lib.platforms.linux;
+  };
+}
